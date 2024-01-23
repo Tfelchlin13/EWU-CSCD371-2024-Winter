1bd7d735a43d173b63dc963404b0678492421a72
Trevor Felchlin <tfelchlin@ewu.edu>
2024-01-22 19:09:41 -0800

    Test: test added for CheckingForClassNameProperty

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -5,5 +5,15 @@ namespace Logger.Tests;
 [TestClass]
 public class LogFactoryTests
 {
+    [TestMethod]
+    public void CreateLogger_SetsClassNameUsingObjectInitializer_Success()
+    {
+        var logFac = new LogFactory();
+        var expectedclassName = "LogFactory";
+        var logger = logger.CreateLogger(expectedclassName);
+
+        Assert.IsNotNull(logger);
+        Assert.AreEqual(expectedclassName, logger.ClassName);
+    }
 
 }
