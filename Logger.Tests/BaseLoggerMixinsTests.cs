bb00aa32c01ded5ec66eba701c5347c9071a009f
Tfelchlin <81547488+Tfelchlin@users.noreply.github.com>
2024-01-25 18:54:06 -0800

    Added tests to BaseLoggerMixins
    I should have done this sooner but was lost till i read more and watched more lectures

--- Logger.Tests/BaseLoggerMixinsTests.cs
+++ Logger.Tests/BaseLoggerMixinsTests.cs
@@ -7,17 +7,50 @@ namespace Logger.Tests;
 [TestClass]
 public class BaseLoggerMixinsTests
 {
-    /*[TestMethod]
+    [TestMethod]
     [ExpectedException(typeof(ArgumentNullException))]
     public void Error_WithNullLogger_ThrowsException()
     {
-        
+
 
         // Act
         BaseLoggerMixins.Error(null, "");
 
         // Assert
-    }*/
+    }
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Warning_WithNullLogger_ThrowsException()
+    {
+
+
+        // Act
+        BaseLoggerMixins.Warning(null, "");
+
+        // Assert
+    }
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Information_WithNullLogger_ThrowsException()
+    {
+
+
+        // Act
+        BaseLoggerMixins.Information(null, "");
+
+        // Assert
+    }
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Debug_WithNullLogger_ThrowsException()
+    {
+
+
+        // Act
+        BaseLoggerMixins.Debug(null, "");
+
+        // Assert
+    }
 
     [TestMethod]
     public void Error_WithData_LogsMessage()
@@ -44,6 +77,76 @@ public class BaseLoggerMixinsTests
         Assert.IsTrue(typeof(BaseLogger).GetProperty("ClassName") != null);
     }
 
+    [TestMethod]
+    public void Error_WithData_Successful()
+    {
+         TestLogger logger = new TestLogger();
+        logger.Error("Message {0}", 10, 45, 100, 55);
+        Assert.AreEqual(1, logger.LoggedMessages.Count);
+        Assert.AreEqual(LogLevel.Error, logger.LoggedMessages[0].LogLevel);
+        Assert.AreEqual("BaseLogger Error: Message 10 45 100 55", logger.LoggedMessages[0].Message);
+    }
+    [TestMethod]
+    public void Warning_WithData_Successful()
+    {
+        // Arrange
+        TestLogger logger = new TestLogger();
+
+        // Act
+        logger.Warning("Message {0} {1} {2} {3}", 10, 45, 100, 55);
+
+        // Assert
+        Assert.AreEqual(1, logger.LoggedMessages.Count);
+        Assert.AreEqual(LogLevel.Warning, logger.LoggedMessages[0].LogLevel);
+        Assert.AreEqual("BaseLogger Warning: Message 10 45 100 55", logger.LoggedMessages[0].Message);
+    }
+    [TestMethod]
+    public void Debug_WithData_Successful()
+    {
+        // Arrange
+        TestLogger logger = new TestLogger();
+
+        // Act
+        logger.Debug("Message {0} {1} {2} {3}", 10, 45, 100, 55);
+
+
+        // Assert
+        Assert.AreEqual(1, logger.LoggedMessages.Count);
+        Assert.AreEqual(LogLevel.Debug, logger.LoggedMessages[0].LogLevel); 
+        Assert.AreEqual("BaseLogger Debug: Message 10 45 100 55", logger.LoggedMessages[0].Message);
+
+    }
+    [TestMethod]
+    public void Information_WithData_Successful()
+    {
+        // Arrange
+        TestLogger logger = new TestLogger();
+
+        // Act
+        logger.Information("Message {0} {1} {2} {3}", 10, 45, 100, 55);
+
+        // Assert
+        Assert.AreEqual(1, logger.LoggedMessages.Count);
+        Assert.AreEqual(LogLevel.Information, logger.LoggedMessages[0].LogLevel);
+        Assert.AreEqual("BaseLogger Information: Message 10 45 100 55", logger.LoggedMessages[0].Message);
+    }
+    [TestMethod]
+    public void LogWithLevel_WithNonNullLogger_Success()
+    {
+        // Arrange
+        TestLogger logger = new TestLogger();
+        LogLevel logLevel = LogLevel.Warning;
+        string message = "Test Message {0} {1}";
+        object[] args = { 42, "abc" };
+
+        // Act
+        BaseLoggerMixins.LogWithLevel(logger, logLevel, message, args);
+
+        // Assert
+        Assert.AreEqual(1, logger.LoggedMessages.Count);
+        Assert.AreEqual(logLevel, logger.LoggedMessages[0].LogLevel);
+        Assert.AreEqual("BaseLogger Warning: Test Message 42 abc", logger.LoggedMessages[0].Message);
+    }
 
 }
 
@@ -58,3 +161,4 @@ public class TestLogger : BaseLogger
 
     
 }
+
--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -5,27 +5,27 @@ namespace Logger;
 
     public static class BaseLoggerMixins
     {
-        public static void Error(this BaseLogger logger, string message, params object[] args)
+        public static void Error(this BaseLogger? logger, string message, params object[] args)
         {
             LogWithLevel(logger, LogLevel.Error, message, args);
         }
 
-        public static void Warning(this BaseLogger logger, string message, params object[] args)
+        public static void Warning(this BaseLogger? logger, string message, params object[] args)
         {
             LogWithLevel(logger, LogLevel.Warning, message, args);
         }
 
-        public static void Information(this BaseLogger logger, string message, params object[] args)
+        public static void Information(this BaseLogger? logger, string message, params object[] args)
         {
             LogWithLevel(logger, LogLevel.Information, message, args);
         }
 
-        public static void Debug(this BaseLogger logger, string message, params object[] args)
+        public static void Debug(this BaseLogger? logger, string message, params object[] args)
         {
             LogWithLevel(logger, LogLevel.Debug, message, args);
         }
 
-        private static void LogWithLevel(BaseLogger logger, LogLevel logLevel, string message, params object[] args)
+        public static void LogWithLevel(BaseLogger? logger, LogLevel logLevel, string message, params object[] args)
         {
             if (logger == null)
             {
@@ -37,5 +37,5 @@ namespace Logger;
             string formattedMessage = string.Format(CultureInfo.InvariantCulture, message, args);
             logger.Log(logLevel, $"{className} {logLevel}: {formattedMessage}");
         }
-    }
 
+}
