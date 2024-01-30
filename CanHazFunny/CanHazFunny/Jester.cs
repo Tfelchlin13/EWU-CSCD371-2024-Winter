1a8564a0a81187f59aaa6e12a500cdb20c2a1683
Trevor Felchlin <tfelchlin@ewu.edu>
2024-01-29 19:01:43 -0800

    Fixed jester

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -1,28 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CanHazFunny;
-
+namespace CanHazFunny
+{
     public class Jester(IJokeService jokeService, IJokeOutput jokeOutput)
     {
-        private readonly IJokeService _jokeService = jokeService ?? throw new ArgumentNullException(nameof(jokeService));
-        private readonly IJokeOutput _jokeOuput = jokeOutput ?? throw new ArgumentNullException(nameof(jokeOutput));
-
+        public IJokeService JokeService { get; } = jokeService ?? throw new ArgumentNullException(nameof(jokeService));
+        public IJokeOutput JokeOutput { get; } = jokeOutput ?? throw new ArgumentNullException(nameof(jokeOutput));
 
         public void TellJoke()
         {
             string joke;
             do
             {
-                joke = jokeService.GetJoke();
+                joke = JokeService.GetJoke();
             } while (joke.Contains("Chuck Norris"));
 
-            jokeOutput.WriteJoke(joke);
+            JokeOutput.WriteJoke(joke);
         }
-
     }
-    
-
+}
